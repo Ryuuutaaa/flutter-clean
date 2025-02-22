6f7d132afc4922af80a26c4f8a7895e81780ff1d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-02-22 17:20:56 +0800

    Add ArticleRepository interface with method for fetching news articles

--- lib/features/daily_news/domain/repositories/article_repository.dart
+++ lib/features/daily_news/domain/repositories/article_repository.dart
@@ -1,1 +1,6 @@
+import 'package:clean_arsitecture/core/resource/data_state.dart';
+import 'package:clean_arsitecture/features/daily_news/domain/entities/article.dart';
 
+abstract class ArticleRepository {
+  Future<DataState<List<ArticleEntity>>> getNewsArticel();
+}
